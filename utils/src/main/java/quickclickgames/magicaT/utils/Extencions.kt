ffc69e5f1b291d2761c8fb687c726a9de7c6254e
Moiseev Evgeniy <moiseev.evgeniy.1985@gmail.com>
2023-04-14 12:08:44 +0300

    v2

--- utils/src/main/java/quickclickgames/magicaT/utils/Extencions.kt
+++ utils/src/main/java/quickclickgames/magicaT/utils/Extencions.kt
@@ -26,20 +26,6 @@ fun Activity.jumpToMidasGoldenTouchRoom(level: String, room: String, activity: A
     finish()
 }
 
-fun String.sendDeepTag() {
-    OneSignal.sendTag("key2",
-        this.replace("myapp://", "").substringBefore("/"))
-}
-
-fun MutableMap<String, Any>?.sendAppsTag() {
-    if (this?.get("campaign").toString() != "null") {
-        OneSignal.sendTag("key2",
-            this?.get("campaign").toString().substringBefore("_"))
-    } else {
-        OneSignal.sendTag("key2", "organic")
-    }
-}
-
 fun Activity.isAdb(): Boolean {
     return Settings.Global.getString(
         this.contentResolver,
